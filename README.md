2561b683ab2d8c5a73e968a7dcfeb44a993a6ef1
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-16 16:00:50 -0500

    assignment complete

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+Knowledge Check
+1. Get Request
+2. 89ms
+3. 200 ok -> Means the request was fulfilled
+4. Other and text
--- ajax.js
+++ ajax.js
@@ -1,5 +1,61 @@
 $(document).ready(function () {
 
-  /* Your code goes here */
+  $('#step12 button').on('click', function (){
+    $.ajax({
+      method: 'GET',
+      url: 'http://first-ajax-api.herokuapp.com/',
+      dataType:''
+    });
+  });
+
+  $('#step3456 button').on('click', function (){
+    $.ajax({
+      method: 'GET',
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      dataType: 'text'
+    }).done(function(data) {
+      console.log(data);
+      console.log('We did it yo!')
+      $('section#step3456').append(data);
+    }).fail(function () {
+      $('section#step3456').append('Try harder next time!');
+      console.log('That did not go very well...')
+    }).always(function(){
+      console.log('Hey the request finished!');
+    });
+  });
+
+  $('#step7 button').on('click', function (){
+    $.ajax({
+      method: 'GET',
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      dataType:'text'
+    }).done(function(data){
+      console.log(data);
+    });
+  });
+
+  $('#step8 button').on('click', function (){
+    $.ajax({
+      method: 'GET',
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      dataType:'text',
+      data: {timezone: 'America/Mexico_City'}
+    }).done(function(data){
+      $('section#step8').append(data);
+      console.log(data);
+    });
+  });
+
+  $('#step9 button').on('click', function (){
+    $.ajax({
+      method: 'GET',
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      dataType:'html'
+    }).done(function(data){
+      $('#attribute').append(data);
+      console.log(data);
+    });
+  });
 
 });
